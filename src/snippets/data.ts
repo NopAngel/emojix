6bacded96cb0ac4731b3800ba6abb3d4d70308a2
NoAngel <155915830+Angel45003@users.noreply.github.com>
2024-06-26 16:21:32 -0400

    emoji app

---
+++ src/snippets/data.ts
@@ -0,0 +1,76 @@
+export const jsonData = [
+    {
+        emoji: "🚀",
+        id: 0,
+        title: 'Cohete.',
+        type: 'Cohete'
+    },
+    {
+        emoji: "🙈",
+        id: 1,
+        type: 'Mono',
+        title: 'Mono tapando su Cara.'
+    },
+    {
+        emoji: "😰",
+        id: 2,
+        type: 'Cara',
+        title: 'Asustado.'
+    },
+    {
+        emoji: "🐍",
+        id: 3,
+        type: 'Reptil',
+        title: 'Serpiente.'
+    },{
+        emoji: "🐈‍⬛",
+        type: 'Animal',
+        id: 4,
+        title: 'Gato.'
+    },{
+        emoji: "🍗",
+        type: 'Comida',
+        id: 5,
+        title: 'Pollo Frito.'
+    },{
+        emoji: "🔥",
+        type: 'Fuego',
+        id: 6,
+        title: 'Fuego.'
+    },{
+        emoji: "🐔",
+        type: 'Animal',
+        id: 7,
+        title: 'Pollo.'
+    },{
+        emoji: "✨",
+        type: 'Particula',
+        id: 8,
+        title: 'Particulas.'
+    },{
+        emoji: "👍",
+        type: 'Dedo',
+        id: 9,
+        title: 'Like.'
+    },{
+        emoji: "🤣",
+        id: 10,
+        type: 'Cara',
+        title: 'Risas.'
+    },{
+        emoji: "😎",
+        id: 11,
+        type: "Cara",
+        title: 'Pro.'
+    },{
+        emoji: "💀",
+        type: 'Cohete',
+        id: 12,
+        title: 'Calavera.'
+    },{
+        emoji: "👻",
+        type: 'Cohete',
+        id: 13,
+        title: 'Fantasma.'
+    }
+]
